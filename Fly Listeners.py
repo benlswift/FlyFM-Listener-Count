968fc0fbbf13b983b198d281161d2272d0dd246e
benlswift <ben.swift2016@my.ntu.ac.uk>
2019-05-27 15:13:58 +0100

    Added simple GUI

--- Fly Listeners.py
+++ Fly Listeners.py
@@ -2,8 +2,12 @@ from urllib.request import urlopen
 from xml.dom import minidom
 from datetime import datetime
 import time
-
+import tkinter as tk
+window = tk.Tk()
+window.title("FLY LISTENERS")
+labelfont = ('times', 20, 'bold')
 while(1):
+
     currentDT = datetime.now()
     hour = currentDT.hour
     minute = currentDT.minute
@@ -12,6 +16,8 @@ while(1):
     h = p.decode("utf-8")
     res = h.find("Current Listeners:") + 19
     print (hour,":",minute, " ","Current Listeners: ", h[res],h[res+1])
+    label = tk.Label(window, text="Current Listeners: " + h[res],font=("Helvetica", 16)).pack()
+    window.update()
     if ( 29 < minute < 39):
         datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         dayTime =currentDT.strftime("%A %d-%m-%Y %H00")
@@ -19,4 +25,5 @@ while(1):
         f= open("log.txt","a")
         f.write(log)
         f.close()
-    time.sleep(600)
+    #getListeners()
+    time.sleep(10)
